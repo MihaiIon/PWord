fecd72c8f6863a2f3f71750df9e66cf59fcd9f5c
Mihai Ionescu <mihai.t.ionescu@gmail.com>
2017-05-20 23:25:43 -0400

    Added Word and Letter constructor

--- js/src/core/setWordList.js
+++ js/src/core/setWordList.js
@@ -4,6 +4,6 @@
 PWord.fn.setWordList = function( list ){
 	var c = this.core.wordList = [];
  	for (var i = 0; i < list.length; i++) {
- 		c.push( new this.Word(list[i]) );
+ 		c.push( new this.Word( list[i].word, list[i].color ) );
  	}	
 };
--- js/src/model/word.js
+++ js/src/model/word.js
@@ -1,6 +1,17 @@
 /*
  *
  */
-PWord.fn.Word = function(){
-	
+PWord.fn.Word = function( str, color ){
+	"use strict";
+	// 
+	this.input = str;
+
+	// 
+	this.chars = str.split("");
+	for (var i = 0; i < this.chars.length; i++) {
+		this.chars[i] = new PWord.fn.Letter(this.chars[i].toLowerCase());
+	}
+
+	// Simple way to test if a color is valid.
+	this.color = color && /^#(?:[a-z0-9]{3}){1,2}$/.test(color) ? color : "#000";
 };
