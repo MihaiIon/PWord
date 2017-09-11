e786836033f0cb1963ef1daa65ef269fb2c583a7
Mihai Ionescu <mihai.t.ionescu@gmail.com>
2017-09-11 00:12:09 -0400

    Added 'injectEffect' method. Work in progress.

---
+++ js/src/action/injectEffect.js
@@ -0,0 +1,14 @@
+define( [ "effects" ], function( FX ) {
+	return function( fx_name, data ) {
+		switch( fx_name )
+		{
+			case "changeColor":
+			case "changeSize":
+			case "changeOpacity":
+			case "bump":
+			case "fadeIn":
+			case "fadeOut":
+			default :
+		}
+	}
+} );
