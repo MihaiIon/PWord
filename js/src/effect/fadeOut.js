ece902aa6133f840c0f5e21a83f25f25d176e548
Mihai Ionescu <mihai.t.ionescu@gmail.com>
2017-09-11 00:27:07 -0400

    Fixed Dependencies.

--- js/src/effect/fadeOut.js
+++ js/src/effect/fadeOut.js
@@ -1,4 +1,4 @@
-define( [ "./changeOpacity" ], function( helper, changeOpacity ) {
+define( [ "./changeOpacity" ], function( changeOpacity ) {
 	return function( step ) {
 		return changeOpacity({ value: 0, step: step ? step : 0.01 });
 	};
