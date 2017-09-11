3e447a54eb838812f45be7bcd0830f238d1b1c9a
Mihai Ionescu <mihai.t.ionescu@gmail.com>
2017-09-11 00:26:01 -0400

    Added case when opacity target value is 0.

--- js/src/effect/changeOpacity.js
+++ js/src/effect/changeOpacity.js
@@ -3,12 +3,15 @@ define( [
 	"./base/opacity_func" 
 ], function( helper, opacity_func ) {
 	return function( args ) {
+			
 		return {
 
 			// Basic information on the effect's goal.
 			data: {
 				step: args && args.step ? args.step : helper.rand( 0.04, 0.1 ),
-				targetValue: args && args.value ? args.value : helper.rand( 0.2, 0.9 )
+				targetValue: args && ( args.value || args.value == 0 ) 
+					? args.value 
+					: helper.rand( 0.2, 0.9 )
 			},
 
 			// The core of the effect.
@@ -18,6 +21,11 @@ define( [
 						data: fx.data, 
 						func: fx.func 
 					});
+				}
+
+				// If the particle fades away
+				if ( particle.opacity == 0 ) {
+					particle.consumed = true;
 				}
 			}
 		};
