c1ba5f2bafc99c1248aa8ab27999eb2fdb42ef63
Mihai Ionescu <mihai.t.ionescu@gmail.com>
2017-09-10 23:27:39 -0400

    Added new effect : Fade In.

--- js/src/effects.js
+++ js/src/effects.js
@@ -1,13 +1,15 @@
 define( [
-	'effect/changeSize',
-	'effect/changeOpacity',
-	'effect/changeColor',
-	'effect/bump'
-], function( size, opacity, color, bump ){
+	"effect/changeSize",
+	"effect/changeOpacity",
+	"effect/changeColor",
+	"effect/bump",
+	"effect/fadeIn"
+], function( size, opacity, color, bump, fadeIn ){
 	return {
 		changeSize: size,
 		changeOpacity: opacity,
 		changeColor: color,
-		bump: bump
+		bump: bump,
+		fadeIn: fadeIn
 	};
 } );
